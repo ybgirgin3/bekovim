a01a4495f219616982f1ad6bc619e8cf4bd815f3
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-11-30 10:48:25 +0300

    craftzdog colorscheme for nvim

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,15 +1,25 @@
+-- local M = {
+--     'navarasu/onedark.nvim',
+--     lazy = false,
+--     priority = 1000,
+-- }
+
 local M = {
-    'navarasu/onedark.nvim',
+    'craftzdog/solarized-osaka.nvim',
     lazy = false,
-    priority = 1000,
+
 }
 
 function M.config()
-    -- vim.cmd.colorscheme('onedark')
-    require('onedark').setup {
-        style = 'deep'
+    require('solarized-osaka').setup {
+        transparent = true
     }
-    require('onedark').load()
+    require('solarized-osaka').load()
+    -- vim.cmd.colorscheme('onedark')
+    -- require('onedark').setup {
+    --     style = 'deep'
+    -- }
+    -- require('onedark').load()
 end
 
 return M
