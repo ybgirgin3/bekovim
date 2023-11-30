25176448c640d36ab1dce771b193462f4d37d2ae
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-11-30 14:08:08 +0300

    tmux session name
    change tmux session name when enters vim, and change to default after leave the vim

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -1,3 +1,4 @@
+
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   callback = function()
     vim.cmd "set formatoptions-=cro"
@@ -63,3 +64,21 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 -- undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m]"]])
 vim.cmd([[let &t_Ce = "\e[4:0m]"]])
+
+
+-- update tmux tab name
+vim.api.nvim_create_autocmd({'BufReadPost', 'FileReadPost', 'BufNewFile', 'BufEnter'}, {
+    pattern = { "*" },
+    callback = function ()
+        vim.cmd(':silent !tmux rename-window bekovim_' .. vim.fn.expand('%:t'))
+        -- vim.cmd(string.format(':silent !tmux rename-window (%s)'), vim.fn.expand('%:t'))
+    end
+})
+
+-- rename to default after leave vim
+vim.api.nvim_create_autocmd({'VimLeave'}, {
+    pattern = { "*" },
+    callback = function ()
+        vim.cmd(":!tmux set-window-option automatic-rename on")
+    end
+})
