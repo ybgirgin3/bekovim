4ae87e967c9d0983fa2bdc2ca72a5ee6f5bb21ba
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-11-30 22:51:58 +0300

    toggle term and other non-working plugins deleted

--- lua/user/launch.lua
+++ lua/user/launch.lua
@@ -36,55 +36,55 @@ LAZY_PLUGIN_SPEC = {
     -- import any extras modules here
     -- { import = "plugins" },
 
-    {
-    "nvimtools/none-ls.nvim",
-    enabled = false,
-    dependencies = { "mason.nvim" },
-    event = { "BufReadPre", "BufNewFile" },
-    opts = function(_, opts)
-      local null_ls = require("null-ls")
-      local formatting = null_ls.builtins.formatting
-      local diagnostics = null_ls.builtins.diagnostics
-      local code_actions = null_ls.builtins.code_actions
-
-      local sources = {
-        -- list of supported sources:
-        -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
-
-        -- python
-        formatting.black.with({
-          filetypes = { "python" },
-          command = prefer_bin_from_venv("black"),
-        }),
-        diagnostics.mypy.with({
-          filetypes = { "python" },
-          command = prefer_bin_from_venv("mypy"),
-        }),
-
-        -- lua
-        formatting.stylua.with({
-          extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
-        }),
-
-        -- shell
-        formatting.shfmt,
-        diagnostics.shellcheck,
-        code_actions.shellcheck,
-
-      }
-
-      -- extend opts.sources
-      for _, source in ipairs(sources) do
-        table.insert(opts.sources, source)
-      end
-
-      -- always remove from opts.sources (e.g. added by lazy.lua extras)
-      local remove_sources = { "goimports_reviser" }
-      opts.sources = vim.tbl_filter(function(source)
-        return not vim.tbl_contains(remove_sources, source.name)
-      end, opts.sources)
-    end,
-  }
+  --   {
+  --   "nvimtools/none-ls.nvim",
+  --   enabled = false,
+  --   dependencies = { "mason.nvim" },
+  --   event = { "BufReadPre", "BufNewFile" },
+  --   opts = function(_, opts)
+  --     local null_ls = require("null-ls")
+  --     local formatting = null_ls.builtins.formatting
+  --     local diagnostics = null_ls.builtins.diagnostics
+  --     local code_actions = null_ls.builtins.code_actions
+  --
+  --     local sources = {
+  --       -- list of supported sources:
+  --       -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+  --
+  --       -- python
+  --       formatting.black.with({
+  --         filetypes = { "python" },
+  --         command = prefer_bin_from_venv("black"),
+  --       }),
+  --       diagnostics.mypy.with({
+  --         filetypes = { "python" },
+  --         command = prefer_bin_from_venv("mypy"),
+  --       }),
+  --
+  --       -- lua
+  --       formatting.stylua.with({
+  --         extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+  --       }),
+  --
+  --       -- shell
+  --       formatting.shfmt,
+  --       diagnostics.shellcheck,
+  --       code_actions.shellcheck,
+  --
+  --     }
+  --
+  --     -- extend opts.sources
+  --     for _, source in ipairs(sources) do
+  --       table.insert(opts.sources, source)
+  --     end
+  --
+  --     -- always remove from opts.sources (e.g. added by lazy.lua extras)
+  --     local remove_sources = { "goimports_reviser" }
+  --     opts.sources = vim.tbl_filter(function(source)
+  --       return not vim.tbl_contains(remove_sources, source.name)
+  --     end, opts.sources)
+  --   end,
+  -- }
 }
 
 function spec(item)
