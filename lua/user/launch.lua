0cf0522884ab53b9ace88e29751405b31f320b7d
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-11-30 12:22:17 +0300

    no lazyspec
    converts neovim to bloated

--- lua/user/launch.lua
+++ lua/user/launch.lua
@@ -1,4 +1,19 @@
-LAZY_PLUGIN_SPEC = {}
+LAZY_PLUGIN_SPEC = {
+    -- add LazyVim and import its plugins
+    -- {
+    --     "LazyVim/LazyVim",
+        -- import = "lazyvim.plugins",
+    --     opts = {
+    --         colorscheme = "solarized-osaka",
+    --         news = {
+    --             lazyvim = true,
+    --             neovim = true,
+    --         },
+    --     },
+    -- },
+    -- import any extras modules here
+    -- { import = "plugins" },
+}
 
 function spec(item)
   table.insert(LAZY_PLUGIN_SPEC, { import = item })
