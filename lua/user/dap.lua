7e12dd1152e1d98deb4108787796150f4027e3a4
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-12-05 18:10:42 +0300

    debugger for go added

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -61,6 +61,36 @@ function M.config()
         end;
       },
     }
+
+    dap.adapters.go = function(callback, config)
+      local handle
+      local pid_or_err
+      local port = 38697
+      handle, pid_or_err = vim.loop.spawn('dlv', {
+        args = { 'dap', '-l', '127.0.0.1:' .. port },
+        detached = true,
+      }, function(code)
+        handle:close()
+        print('Delve exited with exit code: ' .. code)
+      end)
+      -- Wait 100ms for delve to start
+      vim.defer_fn(function()
+        dap.repl.open()
+        callback({ type = 'server', host = '127.0.0.1', port = port })
+      end, 100)
+    end
+
+    dap.configurations.go = {
+      { type = 'go', name = 'Debug', request = 'launch', program = '${file}' },
+      {
+        type = 'go',
+        name = 'Debug test',
+        request = 'launch',
+        mode = 'test', -- Mode is important
+        program = '${file}',
+      },
+    }
+
 end
 
 
