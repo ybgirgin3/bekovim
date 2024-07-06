7a38e1407e6f813596b4334b8aeeddc74989e57c
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2024-07-06 12:56:06 +0300

    return to vs code theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -4,11 +4,11 @@
 --     priority = 1000,
 -- }
 
--- local M = {
--- 	"askfiy/visual_studio_code",
--- 	lazy = false,
--- 	priority = 1000,
--- }
+local M = {
+	"askfiy/visual_studio_code",
+	lazy = false,
+	priority = 1000,
+}
 
 -- local M = {
 --   "eldritch-theme/eldritch.nvim",
@@ -22,18 +22,18 @@
 --     lazy = false,
 -- }
 
-local M = {
-	"Mofiqul/dracula.nvim",
-	priority = 1000,
-	lazy = false,
-}
+-- local M = {
+-- 	"Mofiqul/dracula.nvim",
+-- 	priority = 1000,
+-- 	lazy = false,
+-- }
 
 function M.config()
 	-- DEFAULT ONE
 	-- vim.cmd.colorscheme('koehler')
 
 	-- dracula
-	require("dracula").load()
+	-- require("dracula").load()
 	-- require("dracula").setup({
 	-- 	-- customize dracula color palette
 	-- 	colors = {
@@ -100,43 +100,43 @@ function M.config()
 	-- require('onedark').load()
 
 	-- VSCODE
-	-- vim.cmd.colorscheme("visual_studio_code")
-	-- require("visual_studio_code").setup({
-	-- 	-- `dark` or `light`
-	-- 	mode = "dark",
-	-- 	-- Whether to load all color schemes
-	-- 	preset = true,
-	-- 	-- Whether to enable background transparency
-	-- 	transparent = false,
-	-- 	-- Whether to apply the adapted plugin
-	-- 	expands = {
-	-- 		hop = true,
-	-- 		dbui = true,
-	-- 		lazy = true,
-	-- 		aerial = true,
-	-- 		null_ls = true,
-	-- 		nvim_cmp = true,
-	-- 		gitsigns = true,
-	-- 		which_key = true,
-	-- 		nvim_tree = true,
-	-- 		lspconfig = true,
-	-- 		telescope = true,
-	-- 		bufferline = true,
-	-- 		nvim_navic = true,
-	-- 		nvim_notify = true,
-	-- 		vim_illuminate = true,
-	-- 		nvim_treesitter = true,
-	-- 		nvim_ts_rainbow = true,
-	-- 		nvim_scrollview = true,
-	-- 		nvim_ts_rainbow2 = true,
-	-- 		indent_blankline = true,
-	-- 		vim_visual_multi = true,
-	-- 	},
-	-- 	hooks = {
-	-- 		before = function(conf, colors, utils) end,
-	-- 		after = function(conf, colors, utils) end,
-	-- 	},
-	-- })
+	vim.cmd.colorscheme("visual_studio_code")
+	require("visual_studio_code").setup({
+		-- `dark` or `light`
+		mode = "dark",
+		-- Whether to load all color schemes
+		preset = true,
+		-- Whether to enable background transparency
+		transparent = false,
+		-- Whether to apply the adapted plugin
+		expands = {
+			hop = true,
+			dbui = true,
+			lazy = true,
+			aerial = true,
+			null_ls = true,
+			nvim_cmp = true,
+			gitsigns = true,
+			which_key = true,
+			nvim_tree = true,
+			lspconfig = true,
+			telescope = true,
+			bufferline = true,
+			nvim_navic = true,
+			nvim_notify = true,
+			vim_illuminate = true,
+			nvim_treesitter = true,
+			nvim_ts_rainbow = true,
+			nvim_scrollview = true,
+			nvim_ts_rainbow2 = true,
+			indent_blankline = true,
+			vim_visual_multi = true,
+		},
+		hooks = {
+			before = function(conf, colors, utils) end,
+			after = function(conf, colors, utils) end,
+		},
+	})
 
 	-- vim.cmd.colorscheme('eldritch')
 	-- require("eldritch").setup({
